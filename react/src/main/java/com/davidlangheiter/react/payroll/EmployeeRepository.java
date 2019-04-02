593bc1d1e69036a65e21abc1de40b03ebfe07e57
David Langheiter <david@langheiter.com>
2019-04-02 15:54:43 +0200

    First working react enviorment

--- react/src/main/java/com/davidlangheiter/react/payroll/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.davidlangheiter.react.payroll;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-
-}
